258fc2cf92d693989957bea6ab32194e89c27c10
Your Name <you@example.com>
2024-10-30 15:45:31 +0530

    v3 completed

--- src/main/java/com/capstonebackend/repository/BloodUnitRepository.java
+++ src/main/java/com/capstonebackend/repository/BloodUnitRepository.java
@@ -3,8 +3,12 @@ package com.capstonebackend.repository;
 import com.capstonebackend.enity.BloodUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BloodUnitRepository extends JpaRepository<BloodUnit, Long> {
     Optional<BloodUnit> findById(Long id);
+
+    // Method to find blood units by blood bank name
+    List<BloodUnit> findByBbName(String bbName);
 }
