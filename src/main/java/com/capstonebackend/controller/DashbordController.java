43e295eeedddf7b742dbe7e3e8163549989f039b
Your Name <you@example.com>
2024-11-12 21:25:04 +0530

    search

---
+++ src/main/java/com/capstonebackend/controller/DashbordController.java
@@ -0,0 +1,40 @@
+package com.capstonebackend.controller;
+
+import com.capstonebackend.enity.BloodBankUser;
+import com.capstonebackend.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DashbordController {
+
+    @Autowired
+    private BloodBankUserRepository bloodBankUserRepository;
+
+//    @GetMapping("/location")
+//    public ResponseEntity<List<BloodBankUser>> getRequestsByBloodBank(@RequestParam String address) {
+//        String lowerCaseAddress = address.toLowerCase();
+//        List<BloodBankUser> requests = bloodBankUserRepository.findBloodBankDTOByAddress(lowerCaseAddress);
+//
+//        if (requests.isEmpty()) {
+//            return ResponseEntity.noContent().build();  // No requests found
+//        }
+//        return ResponseEntity.ok(requests);  // Return the list of requests
+//    }
+    @GetMapping("/location")
+    public ResponseEntity<List<BloodBankUser>> getRequestsByBloodBank(@RequestParam String address) {
+        String lowerCaseAddress = address.toLowerCase();
+        List<BloodBankUser> requests = bloodBankUserRepository.findBloodBankDTOByAddress(lowerCaseAddress);
+
+        if (requests.isEmpty()) {
+            return ResponseEntity.noContent().build();  // No requests found
+        }
+        return ResponseEntity.ok(requests);  // Return the list of requests
+    }
+
+
+}
--- src/main/java/com/capstonebackend/repository/BloodBankUserRepository.java
+++ src/main/java/com/capstonebackend/repository/BloodBankUserRepository.java
@@ -4,8 +4,11 @@ import com.capstonebackend.enity.BloodBankUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BloodBankUserRepository extends JpaRepository<BloodBankUser, Long> {
     BloodBankUser findByEmail(String email);
+    List<BloodBankUser> findBloodBankDTOByAddress(String address);
 }
 
